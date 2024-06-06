287ac925e045ca93c92a50b16e624b0c52970c7d
Lokesh Rangarajan <lokesh.rangarajan@assettl.com>
2024-06-07 00:25:48 +0530

    Home Screen Top bar

--- app/src/main/java/com/lokesh/readerapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/lokesh/readerapp/screens/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,8 +26,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.google.firebase.auth.FirebaseAuth
 import com.lokesh.readerapp.R
 import com.lokesh.readerapp.components.ReaderApp
+import com.lokesh.readerapp.navigation.Screens
 
 @Composable
 fun HomeScreen(navigation: NavHostController) {
@@ -59,27 +62,48 @@ fun ReaderTopBar(
     showProfile: Boolean = true,
     navigation: NavHostController
 ) {
-    TopAppBar(title = {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 10.dp)
-        ) {
-            if (showProfile) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_splash_logo),
-                    contentDescription = stringResource(
-                        R.string.app_logo
-                    ),
-                    modifier = Modifier.size(25.dp).scale(1.2f)
+    TopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            ) {
+                if (showProfile) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_splash_logo),
+                        contentDescription = stringResource(
+                            R.string.app_logo
+                        ),
+                        modifier = Modifier
+                            .size(25.dp)
+                            .scale(1.2f)
+                    )
+                    Spacer(modifier = Modifier.width(15.dp))
+                }
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineMedium
                 )
-                Spacer(modifier = Modifier.width(15.dp))
             }
-            Text(
-                text = title,
-                style = MaterialTheme.typography.headlineMedium
-            )
+        },
+        actions = {
+            IconButton(
+                onClick = {
+                    FirebaseAuth.getInstance().signOut().run {
+                        navigation.navigate(Screens.LoginScreen)
+                    }
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_exit),
+                    contentDescription = stringResource(
+                        R.string.logout_icon
+                    ),
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
         }
-    })
+    )
 }
 
 @Composable
